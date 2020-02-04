e5760a6ce3865bc2f46a61fd94d83dfe05686c32
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2020-02-04 16:07:12 -0300

    Initial Commit

---
+++ BatMobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BatMobile
+//
+//  Created by Ailton Vieira Pinto Filho on 04/02/20.
+//  Copyright © 2020 Veevaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
